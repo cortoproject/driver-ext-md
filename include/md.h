fd0e5442b34dfae60cae12beefd60611439e1333
Sander Mertens <sander.mertens8@gmail.com>
2018-02-18 13:38:54 -0800

    Update to new language binding

--- include/md.h
+++ include/md.h
@@ -1,5 +1,4 @@
 /* md.h
- *
  * This is the main package file. Include this file in other projects.
  * Only modify inside the header-end and body-end sections.
  */
@@ -7,7 +6,6 @@
 #ifndef DRIVER_EXT_MD_H
 #define DRIVER_EXT_MD_H
 
-#include <corto/corto.h>
 #include <corto/corto.h>
 #include <driver/ext/md/_project.h>
 #include <corto/c/c.h>
@@ -19,6 +17,7 @@
 #include <driver/ext/md/_type.h>
 #include <driver/ext/md/_interface.h>
 #include <driver/ext/md/_load.h>
+#include <driver/ext/md/_binding.h>
 #include <driver/ext/md/c/_api.h>
 
 /* $body() */
